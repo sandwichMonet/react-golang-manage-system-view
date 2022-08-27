5a9644e99e6e8536c2e0cb23b9171b227724f3ff
Zach <fangqing_0812@163.com>
2022-08-27 23:13:36 +0800

    完成Article组件静态页面 todo:动态渲染列表

--- src/pages/Article/index.jsx
+++ src/pages/Article/index.jsx
@@ -1,8 +1,115 @@
+import { Breadcrumb, Button, Card, DatePicker, Form, Radio, Select, Table, Tag, Space } from 'antd'
+import BreadcrumbItem from 'antd/lib/breadcrumb/BreadcrumbItem'
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
+import { Link } from 'react-router-dom'
+import 'moment/locale/zh-cn'
+import locale from 'antd/lib/date-picker/locale/zh_CN'
+import img404 from '@/assets/error.png'
 const Article = () => {
+  const columns = [
+    {
+      title: '封面',
+      dataIndex: 'cover',
+      width: 120,
+      render: cover => <img src={cover || img404} width={80} height={60} alt="" />
+    },
+    {
+      title: '标题',
+      dataIndex: 'title',
+      width: 220
+    },
+    {
+      title: '状态',
+      dataIndex: 'status',
+      render: data => <Tag color="green">审核通过</Tag>
+    },
+    {
+      title: '发布时间',
+      dataIndex: 'pubdate'
+    },
+    {
+      title: '阅读数',
+      dataIndex: 'read_count'
+    },
+    {
+      title: '评论数',
+      dataIndex: 'comment_count'
+    },
+    {
+      title: '点赞数',
+      dataIndex: 'like_count'
+    },
+    {
+      title: '操作',
+      render: data => {
+        return (
+          <Space size="middle">
+            <Button type="primary" shape="circle" icon={<EditOutlined />} />
+            <Button type="primary" danger shape="circle" icon={<DeleteOutlined />} />
+          </Space>
+        )
+      }
+    }
+  ]
+
+  const data = [
+    {
+      id: '8218',
+      comment_count: 0,
+      cover: {
+        images: ['http://geek.itheima.net/resources/images/15.jpg']
+      },
+      like_count: 0,
+      pubdate: '2019-03-11 09:00:00',
+      read_count: 2,
+      status: 2,
+      title: 'wkwebview离线化加载h5资源解决方案'
+    }
+  ]
   return (
-    <>
-      <div>Article</div>
-    </>
+    <div>
+      <Card
+        title={
+          <Breadcrumb separator>
+            <BreadcrumbItem>
+              <Link to="/home">首页</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem>内容管理</BreadcrumbItem>
+          </Breadcrumb>
+        }
+        style={{ marginBottom: 20 }}
+      >
+        <Form initialValues={{ status: null }}>
+          <Form.Item label="状态" name="status">
+            <Radio.Group>
+              <Radio value={null}>全部</Radio>
+              <Radio value={0}>草稿</Radio>
+              <Radio value={1}>待审核</Radio>
+              <Radio value={2}>审核通过</Radio>
+              <Radio value={3}>审核失败</Radio>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item label="频道" name="channel_id">
+            <Select placeholder="请选择文章频道" defaultValue="lucy" style={{ width: 120 }}>
+              <Select.Option value="jack">Jack</Select.Option>
+              <Select.Option value="lucy">Lucy</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item label="日期" name="date">
+            <DatePicker.RangePicker locale={locale}></DatePicker.RangePicker>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" style={{ marginLeft: 80 }}>
+              筛选
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+      {/* table */}
+      <Card title={`根据筛选条件共查询到 count 条结果：`}>
+        <Table rowKey="id" columns={columns} dataSource={data} />
+      </Card>
+    </div>
   )
 }
 
